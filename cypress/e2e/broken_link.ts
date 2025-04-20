180bf6c04d51bf2d2d68d96855567f063a772f83
Stephen Mott <stephen@motts.id.au>
2025-04-20 15:53:21 +0800

    fix broken link

--- cypress/e2e/broken_link.ts
+++ cypress/e2e/broken_link.ts
@@ -1,16 +1,15 @@
-const sites = ['/en/work', '/en/projects'];
+const sites = ['/en/work', '/en/projects','recruiter'];
 
 describe('Included links', () => {
   it('check all links don\'t 404', () => {
       for (const p of sites) {
         cy.visit(p);
         cy.get('a').each(page => {
+          // I have other a links on the page, only show http(s)
           if (page.prop('href').startsWith('http')) {
-            // Cypress is being weird when I try to use href instead
-            // 3rdwavemedia keeps 500ing which results in a broken build for me
-            // would prefer to keep attribution footer as is, so skip any links that has Xiaoyings name on them
-            // BHP also seems to from githubs IP range.
-            if (!page.contents().text().includes('Xiaoying Riley') || !page.prop('href').includes('bhp.com')) {
+            // Two websites seem to be inconsistent with their 404 pages, so we'll just skip them for now.'
+            if (!page.contents().text().includes('Xiaoying Riley') && !page.prop('href').includes('bhp.com'))
+            {
               cy.request(page.prop('href')).should('have.property', 'status', 200);
             }
           }
