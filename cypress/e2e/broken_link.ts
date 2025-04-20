847f1269112dd504725a43e8e74812c3d64a815c
Stephen Mott <stephen@motts.id.au>
2025-04-20 16:00:36 +0800

    recruiter test, ignore sites that are inconsistently reporting

--- cypress/e2e/broken_link.ts
+++ cypress/e2e/broken_link.ts
@@ -7,8 +7,10 @@ describe('Included links', () => {
         cy.get('a').each(page => {
           // I have other a links on the page, only show http(s)
           if (page.prop('href').startsWith('http')) {
-            // Two websites seem to be inconsistent with their 404 pages, so we'll just skip them for now.'
-            if (!page.contents().text().includes('Xiaoying Riley') && !page.prop('href').includes('bhp.com'))
+            // Three websites seem to be inconsistent with their 404 pages, so we'll just skip them for now.'
+            if (!page.contents().text().includes('Xiaoying Riley')
+              && !page.prop('href').includes('bhp.com')
+              && !page.prop('href').includes('element14.com'))
             {
               cy.request(page.prop('href')).should('have.property', 'status', 200);
             }
